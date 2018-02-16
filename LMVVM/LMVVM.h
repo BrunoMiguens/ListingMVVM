b8f9890d343924dceba5fe50ced21592285d05dd
Bruno Miguens <bruno.miguens@olx.com>
2018-02-16 13:50:31 +0000

    Initial Commit

---
+++ LMVVM/LMVVM.h
@@ -0,0 +1,19 @@
+//
+//  LMVVM.h
+//  LMVVM
+//
+//  Created by bruno.miguens on 16/02/2018.
+//  Copyright © 2018 Bruno Miguêns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LMVVM.
+FOUNDATION_EXPORT double LMVVMVersionNumber;
+
+//! Project version string for LMVVM.
+FOUNDATION_EXPORT const unsigned char LMVVMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LMVVM/PublicHeader.h>
+
+
